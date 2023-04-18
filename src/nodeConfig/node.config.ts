95a66636455df115b44da895838fb4e5e9e1aa9f
collins mbathi <perezcoolins@gmail.com>
2023-04-18 07:48:46 +0300

    fixed it all

--- src/nodeConfig/node.config.ts
+++ src/nodeConfig/node.config.ts
@@ -6,7 +6,7 @@ export const nodeConfig = {
 	env: {
 		port: process.env.PORT || 5000,
 		secretKey: process.env.SECRET_KEY || 'secretkeythatisnotpublic',
-		mongoUrl:process.env.MONGO_URL || 'mongodb+srv://mbathi:shanicecole@cluster0.hex8l.mongodb.net/Grocery_Api?retryWrites=true&w=majority',
+		mongoUrl:process.env.MONGO_URL || 'mongodb+srv://mbathi:shanicecole@cluster0.hex8l.mongodb.net/Ecommerce-solo?retryWrites=true&w=majority',
 	    cloud_name: "mbathi",   
         api_key: "977179525928855",
         api_secret: "P5I6ukSyTe0u7Dcb_pJPhf-Bmv8",
